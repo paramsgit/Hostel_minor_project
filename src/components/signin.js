eca51b5d6d1db724228fd3d51ea2342f44dc95b5
Sbsbsv <sbsbsv002@gmail.com>
2023-04-22 15:30:04 +0530

    updated hamburger

--- src/components/signin.js
+++ src/components/signin.js
@@ -62,7 +62,7 @@ export const Signin = (props) => {
    <>
    <div className="container signinbox">
    <section className="bg-gray-50 dark:bg-gray-900 siginsection">
-  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 sectonexdiv">
       <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
           Mega Boys Hostel    
--- src/components/tsidebar.js
+++ src/components/tsidebar.js
@@ -217,7 +217,7 @@ axios.post("http://localhost:5000/api/a/newupload", formData, {headers: {
   return (
    <>
   
-    <div className="hamdiv">
+    <div className={`hamdiv ${state.value?'flex':'displaynone'}`}>
    <button id="hambutton" className={`colbtn `} onClick={anotherclick}>
 
    <div id="hb1" className={`bar-top`}></div>
