2b283061556e44e21c90d841ce220b209bd4c9f3
Sbsbsv <sbsbsv002@gmail.com>
2023-08-27 22:33:47 +0530

    mb2

--- src/components/signup.js
+++ src/components/signup.js
@@ -58,7 +58,7 @@ const handlesubmit =async (e)=>{
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 sectonexdiv">
       <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
-          Flowbite    
+          Mega Boys Hostel    
       </a>
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
